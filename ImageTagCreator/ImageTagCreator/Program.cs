cf0625ce8957f4e207e43a3323390a4177a4787d
satoshi-maemoto <peugeot.106.s16@gmail.com>
2015-07-12 17:36:44 +0900

    first commit

---
+++ ImageTagCreator/ImageTagCreator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTagCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var targets = new string[]
+            {
+                ".JPG",
+                ".JPEG",
+                ".GIF",
+            };
+
+            var template = "<p><img alt=\"\" src=\"/files/u/blog/maemoto/{0}/{1}\" /><br />&nbsp;</p>";
+            var currentDirectory = Path.GetFileName(Directory.GetCurrentDirectory());
+            Console.WriteLine(currentDirectory);
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            foreach (var file in files)
+            {
+                var match = false;
+                foreach (var target in targets)
+                {
+                    if (file.ToUpper().EndsWith(target))
+                    {
+                        match = true;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    Console.WriteLine(string.Format(template, currentDirectory, Path.GetFileName(file)));
+                }
+            }
+        }
+    }
+}
